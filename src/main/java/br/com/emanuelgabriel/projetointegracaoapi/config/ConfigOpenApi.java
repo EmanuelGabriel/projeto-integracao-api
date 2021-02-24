caefdaf13dfccb75a9b6264f9ace2b5714a6af96
Samuel Gabriel Sousa <emanuel.gabriel.sousa@protonmail.com>
2021-02-24 15:21:51 -0300

    documentando o CepResource com o OpenAPI

--- src/main/java/br/com/emanuelgabriel/projetointegracaoapi/config/ConfigOpenApi.java
+++ src/main/java/br/com/emanuelgabriel/projetointegracaoapi/config/ConfigOpenApi.java
@@ -1,4 +1,18 @@
 package br.com.emanuelgabriel.projetointegracaoapi.config;
 
-public class ConfigOpenApi {
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springdoc.webmvc.ui.SwaggerConfig;
+
+@OpenAPIDefinition(
+        info = @Info(title = "Consumer de API REST externa", description = "Documentação da API REST - CEP",
+        termsOfService = "Termos de uso em construção",
+        version = "1.0.0",
+        contact = @Contact(name = "Emanuel Gabriel Sousa", email = "emanuel.gabriel.sousa@hotmail.com",
+        url = "https://emanuelgabriel.github.io/")
+        )
+)
+public class ConfigOpenApi extends SwaggerConfig {
 }
